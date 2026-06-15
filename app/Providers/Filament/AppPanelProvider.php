70ac01cc3e066be7734693f084b5c528ac4ed9e4
Faizpi <faizbintang1244@gmail.com>
2026-06-15 14:33:41 +0700

    feat: initialize Filament AppPanelProvider with custom theme, navigation groups, and integration hooks

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -10,7 +10,6 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Navigation\NavigationGroup;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Enums\ThemeMode;

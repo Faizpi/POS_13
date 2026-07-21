2628237025b23f878b6a8fecf30b359ba70c680a
Faizpi <faizbintang1244@gmail.com>
2026-07-21 12:12:34 +0700

    feat: introduce UI polish component for custom Filament styling and sidebar refinements

--- resources/views/filament/components/ui-polish.blade.php
+++ resources/views/filament/components/ui-polish.blade.php
@@ -278,7 +278,7 @@
     }
 
     .fi-sidebar-group-collapse-button {
-        margin-inline-end: 6px !important;
+        margin-inline-end: 10px !important;
     }
 
     .fi-sidebar-group-label > button:focus-visible,

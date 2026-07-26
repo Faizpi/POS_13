8ac67ed877362530ca9186d9af802b30d6a68487
Faizpi <faizbintang1244@gmail.com>
2026-07-26 08:54:00 +0700

    fix(filament): enlarge brand title

--- resources/views/filament/components/brand.blade.php
+++ resources/views/filament/components/brand.blade.php
@@ -7,7 +7,7 @@
         style="display: block; width: 2.5rem; height: 2.5rem; flex: 0 0 2.5rem; object-fit: contain;"
     >
 
-    <span style="font-size: 0.875rem; font-weight: 600; line-height: 1.25rem; color: currentColor;">
+    <span style="font-size: 1rem; font-weight: 700; line-height: 1.5rem; color: currentColor;">
         Hibiscus Efsya
     </span>
 </div>
